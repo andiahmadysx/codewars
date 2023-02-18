1d4b385e9e0f49320e1e9f21165b4c2945c4a041
Andi Ahmad Yusup <andiahmadyusup@gmail.com>
2023-02-18 22:29:56 +0700

    Codewars Solutions

---
+++ hello-world/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println(greet());
+
+    }
+    public static String greet(){
+        return "hello world";
+    }
+
+}
